ee659e8e0a52123a44045d4dc2a5724b5303ffbd
andarutr <andarutr@gmail.com>
2025-03-24 11:42:59 +0700

    P

--- src/app/layouts/login-nik/login-nik.component.ts
+++ src/app/layouts/login-nik/login-nik.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login-nik',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './login-nik.component.css'
 })
 export class LoginNikComponent {
+  nik: string = ''; 
 
+  constructor(private router: Router) {}
+
+  login(nik: string) {
+    if (nik == '105000464') {
+      this.router.navigate(['/home']); 
+    } else {
+      Swal.fire("Gagal","NIK tidak valid!","error"); 
+    }
+  }
 }
