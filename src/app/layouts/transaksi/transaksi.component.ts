220864f8d3e66e1ec5ab550ae5dd762ac4dcb4e2
andarutr <andarutr@gmail.com>
2025-03-25 16:48:03 +0700

    OK

--- src/app/layouts/transaksi/transaksi.component.ts
+++ src/app/layouts/transaksi/transaksi.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-transaksi',
-  imports: [],
   templateUrl: './transaksi.component.html',
-  styleUrl: './transaksi.component.css'
+  styleUrls: ['./transaksi.component.css']
 })
 export class TransaksiComponent {
+  nik: string | null = null;
+  nama: string | null = null;
 
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      this.nik = params['nik'];
+      this.nama = params['nama'];
+    });
+  }
+
+  goBack() {
+    this.router.navigate(['/home'], { queryParams: { nik: this.nik, nama: this.nama } });
+  }
 }
